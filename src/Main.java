9cd9b74157711d24fab252745e77f2309f35c1bb
Denis <demaz@bk.ru>
2022-07-23 10:59:02 +0700

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
 
+        int[] firstArr = new int[3];
+        firstArr[0] = 1;
+        firstArr[1] = 2;
+        firstArr[2] = 3;
+
+        double[] secondArr = {1.57, 7.654, 9.986};
+
+        char[] thirdArr = {'a', 'b', 'c'};
     }
 }
